035839a43ddd0ee572348586ed367ec9182e4a21
Matheus Vidal <matheusvidal140@gmail.com>
2024-08-17 14:53:07 -0300

    Inicio do projeto

---
+++ src/main/java/br/rnp/redesegura/RedeSeguraApplication.java
@@ -0,0 +1,13 @@
+package br.rnp.redesegura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedeSeguraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedeSeguraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/rnp/redesegura/RedeSeguraApplicationTests.java
@@ -0,0 +1,13 @@
+package br.rnp.redesegura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedeSeguraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
